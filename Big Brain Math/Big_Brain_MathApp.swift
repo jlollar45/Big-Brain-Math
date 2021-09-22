aabc7367d896b2eb30a1efad865dc1e0fde66c43
John Lollar <jlollar45@gmail.com>
2021-09-21 19:44:05 -0500

    Initial Commit

---
+++ Big Brain Math/Big_Brain_MathApp.swift
@@ -0,0 +1,17 @@
+//
+//  Big_Brain_MathApp.swift
+//  Big Brain Math
+//
+//  Created by ParkingPal on 9/21/21.
+//
+
+import SwiftUI
+
+@main
+struct Big_Brain_MathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
